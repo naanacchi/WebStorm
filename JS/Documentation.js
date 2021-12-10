d7360a75e98ecbfe77970c25e93399fffa9b0b63
Shizqu <60578896+Shizqu@users.noreply.github.com>
2021-12-10 09:30:35 +0100

    WebStorm

--- JS/Documentation.js
+++ JS/Documentation.js
@@ -1,4 +1,12 @@
 function sum(first, second){
     return fitst + second
 }
+
+
+/**
+ * @description returns sum of two elements
+ * @param first component of sum
+ * @param second component of sum
+ * @return sum of two components
+ */
 console.log(sum(2,3));
