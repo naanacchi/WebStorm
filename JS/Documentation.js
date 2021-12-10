a5ee3330ba1a19236a3b6cfba327a14f42e4ad36
Shizqu <60578896+Shizqu@users.noreply.github.com>
2021-12-10 10:12:25 +0100

    WebStorm

--- JS/Documentation.js
+++ JS/Documentation.js
@@ -1,12 +1,38 @@
-function sum(first, second){
-    return fitst + second
-}
-
-
 /**
  * @description returns sum of two elements
- * @param first component of sum
- * @param second component of sum
- * @return sum of two components
+ * @param {anytype} first component of sum
+ * @param {anytype} second component of sum
+ * @return {anytype} sum of two components
  */
-console.log(sum(2,3));
+
+function sum(first, second)
+{
+    return first + second;
+}
+
+console.log(sum(3,2));
+
+
+// stworz funkcje multiplication, ktora mnozy 3 liczby
+// stworz opis tej funkcji
+// function multiplication (first:Number , second:Number , third:Number): Number
+// returns result of multiplication of three numbers
+
+// opis parametrow
+// params: first component of multiplication
+// param: --
+//p aram -- third comppone,,,,
+//returns result of  multiplication 3 components
+
+/**
+ * @description returns multiplication of three numbers
+ * @param {int} first component of multiplication
+ * @param {int} second component of multiplication
+ * @param {int} third component of multiplication
+ * @return {int} multiplication of three components
+ */
+function mult(first, second, third)
+{
+    return first * second * third;
+}
+console.log(mult(2,4,6));
